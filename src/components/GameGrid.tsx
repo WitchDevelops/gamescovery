175e4a362764fe152fffc0dcd2c6c78a4a40496d
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-09 17:18:09 +0200

    feat: add loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { useGames } from "@/lib/hooks/useGames";
 import { GameCard } from "@/components/GameCard";
 import { SimpleGrid } from "@chakra-ui/react";
+import { GameCardSkeleton } from "@/components/GameCardSkeleton";
 export const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <p>{error}</p>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding={10} spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding={10}
+        spacing={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/lib/hooks/useGames.ts
+++ src/lib/hooks/useGames.ts
@@ -11,22 +11,26 @@ interface FetchGamesResponse {
 export const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((response) => {
         setGames(response.data.results);
+        setIsLoading(false);
       })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
     return () => {
       controller.abort();
     };
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
