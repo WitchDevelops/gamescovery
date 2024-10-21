10a7b352e57e6b3f49f5e9296f81bd2566392418
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-21 21:07:48 +0200

    feat: show skeletons while loading genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,9 +11,10 @@ export const GameGrid = () => {
   return (
     <>
       {error && <p>{error}</p>}
+      
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding={10}
+        paddingX={10}
         spacing={3}
       >
         {isLoading &&
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import { useGenres } from "@/lib/hooks/useGenres";
 import { HStack, Text, List, ListItem, Image } from "@chakra-ui/react";
 import { getCroppedImgUrl } from "@/lib/services/imageResized";
+import { GenreItemSkeleton } from "@/components/GenreItemSkeleton";
 
 export const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
   return (
     <List>
+      {error && null}
+
+      {isLoading &&
+        skeletons.map((skeleton) => <GenreItemSkeleton key={skeleton} />)}
+
       {genres.map((genre) => (
         <ListItem key={genre.id} paddingY={2}>
           <HStack>
