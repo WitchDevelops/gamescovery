eed006d2a9866c876ec2782843c545c0025d330e
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-26 18:19:07 +0200

    make search work

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -3,11 +3,15 @@ import { Image } from "@chakra-ui/react";
 import { ColorModeSwitch } from "@/components/navigation/ColorModeSwitch";
 import { SearchInput } from "@/components/navigation/SearchInput";
 
-export const Navbar = () => {
+interface NavbarProps {
+  onSearch: (searchText: string) => void;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({ onSearch }) => {
   return (
     <HStack p={4} spacing={6}>
       <Image src="/images/logo.webp" alt="logo" boxSize={"60px"} />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/navigation/SearchInput.tsx
+++ src/components/navigation/SearchInput.tsx
@@ -1,13 +1,33 @@
+import React, { useRef } from "react";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 
-export const SearchInput = () => {
+interface SearchInputProps {
+  onSearch: (searchText: string) => void;
+}
+
+export const SearchInput: React.FC<SearchInputProps> = ({ onSearch }) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (searchInputRef.current) {
+      onSearch(searchInputRef.current.value);
+    }
+  };
+
   return (
-    <InputGroup>
-      <InputLeftElement>
-        <BsSearch />
-      </InputLeftElement>
-      <Input placeholder="Search games..." variant="filled" borderRadius={20} />
-    </InputGroup>
+    <form onSubmit={handleSearch}>
+      <InputGroup>
+        <InputLeftElement>
+          <BsSearch />
+        </InputLeftElement>
+        <Input
+          ref={searchInputRef}
+          placeholder="Search games..."
+          variant="filled"
+          borderRadius={20}
+        />
+      </InputGroup>
+    </form>
   );
 };
