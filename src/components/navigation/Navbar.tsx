70298c3afaeb086cf043e6ef68a643e015b179b3
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-26 18:05:05 +0200

    add search component

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import { ColorModeSwitch } from "@/components/navigation/ColorModeSwitch";
+import { SearchInput } from "@/components/navigation/SearchInput";
 
 export const Navbar = () => {
   return (
-    <HStack justifyContent={"space-between"} p={4}>
+    <HStack p={4} spacing={6}>
       <Image src="/images/logo.webp" alt="logo" boxSize={"60px"} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
