69c2d0404e2e5892c0f8ef814384c578bb1c30fd
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-27 19:52:57 +0100

    make logo a link to home

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Stack, HStack, Box } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 import { ColorModeSwitch } from "@/components/navigation/ColorModeSwitch";
 import { SearchInput } from "@/components/navigation/SearchInput";
@@ -12,7 +13,9 @@ export const Navbar: React.FC<NavbarProps> = ({ onSearch }) => {
   return (
     <Stack p={4} spacing={6}>
       <HStack justifyContent={"space-between"}>
-        <Image src="/images/logo_1.png" alt="logo" width={100} height={100} />
+        <Link href="/">
+          <Image src="/images/logo_1.png" alt="logo" width={100} height={100} />
+        </Link>
         <Box
           display={{ base: "none", sm: "block" }}
           width={"100%"}
