368d023666cc9f064b885eb360ee1fe9b5af0ac3
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-27 20:44:04 +0100

    fix console error

--- src/lib/chakra/providers.tsx
+++ src/lib/chakra/providers.tsx
@@ -7,7 +7,7 @@ import { theme } from "@/lib/chakra/theme";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider theme={theme}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
+      <ThemeProvider attribute="class" disableTransitionOnChange={true}>
         {children}
       </ThemeProvider>
     </ChakraProvider>
