5d9ec3a9451e9d5a95fa77b5f5ec1b6ee86bbba3
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-27 21:18:40 +0100

    mock page for testing react-query

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+interface Todo {
+  id: number;
+  title: string;
+}
 export default function Todos() {
-    return (
-        <div>
-            <h1>Todos</h1>
-        </div>
-    );
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      {error && <p>{error}</p>}
+      {todos.length > 0 &&
+        todos.map((todo) => (
+          <div key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
